1f2558b68035db11c56dbdc0732bf0e54361639b
Alexey Rusakov <rusal@bk.ru>
2021-09-29 13:57:23 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace devMessenger
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+      Console.WriteLine("Ежик съел Андрея");
+    }
+  }
+}
