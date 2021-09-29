0eb91dd134732e4e56d7a4948268026ca215bef6
Alexey Rusakov <rusal@bk.ru>
2021-09-29 14:42:38 +0300

    add simple dz1

--- devMessenger/Program.cs
+++ devMessenger/Program.cs
@@ -2,12 +2,16 @@
 
 namespace devMessenger
 {
+  class Ezh
+  {
+    public string name;
+    public string age;
+  }
   class Program
   {
     static void Main(string[] args)
     {
       Console.WriteLine("Hello World!");
-      Console.WriteLine("Русаков съел Ёжика");
     }
   }
 }
