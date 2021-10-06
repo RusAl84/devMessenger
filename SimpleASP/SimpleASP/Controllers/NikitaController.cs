5a832d9c64f1ee0e0514172a66d9852d84e87ef7
Alexey Rusakov <rusal@bk.ru>
2021-10-06 15:46:32 +0300

    simple ASP API

---
+++ SimpleASP/SimpleASP/Controllers/NikitaController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SimpleASP.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class NikitaController : ControllerBase
+  {
+    // GET: api/<NikitaController>
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+      return new string[] { "Ёжик  сьел Никиту" };
+    }
+
+    // GET api/<NikitaController>/5
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+      return "Никита получил " +id.ToString()+ " раз оценку отлично";
+    }
+
+    // POST api/<NikitaController>
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+    }
+
+    // PUT api/<NikitaController>/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    // DELETE api/<NikitaController>/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+  }
+}
