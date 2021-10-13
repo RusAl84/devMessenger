202bdc4b077e8c8b45cdff72daa7924e8f88ebd1
Alexey Rusakov <rusal@bk.ru>
2021-10-13 15:44:48 +0300

    add sound

---
+++ TimerGUI/TimerGUI/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace TimerGUI
+{
+  partial class Form1
+  {
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    ///  Required method for Designer support - do not modify
+    ///  the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.components = new System.ComponentModel.Container();
+      this.label1 = new System.Windows.Forms.Label();
+      this.startButton = new System.Windows.Forms.Button();
+      this.stopButton = new System.Windows.Forms.Button();
+      this.timer1 = new System.Windows.Forms.Timer(this.components);
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.button1 = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Font = new System.Drawing.Font("Segoe UI", 64F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+      this.label1.Location = new System.Drawing.Point(168, 78);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(315, 142);
+      this.label1.TabIndex = 0;
+      this.label1.Text = "10:00";
+      // 
+      // startButton
+      // 
+      this.startButton.Location = new System.Drawing.Point(230, 233);
+      this.startButton.Name = "startButton";
+      this.startButton.Size = new System.Drawing.Size(165, 59);
+      this.startButton.TabIndex = 1;
+      this.startButton.Text = "Запустить";
+      this.startButton.UseVisualStyleBackColor = true;
+      this.startButton.Click += new System.EventHandler(this.startButton_Click);
+      // 
+      // stopButton
+      // 
+      this.stopButton.Location = new System.Drawing.Point(230, 314);
+      this.stopButton.Name = "stopButton";
+      this.stopButton.Size = new System.Drawing.Size(165, 58);
+      this.stopButton.TabIndex = 2;
+      this.stopButton.Text = "Остановить";
+      this.stopButton.UseVisualStyleBackColor = true;
+      this.stopButton.Visible = false;
+      this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+      // 
+      // timer1
+      // 
+      this.timer1.Interval = 1000;
+      this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+      // 
+      // textBox1
+      // 
+      this.textBox1.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+      this.textBox1.Location = new System.Drawing.Point(244, 12);
+      this.textBox1.Name = "textBox1";
+      this.textBox1.Size = new System.Drawing.Size(151, 87);
+      this.textBox1.TabIndex = 3;
+      this.textBox1.Text = "00:03";
+      this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(556, 262);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(94, 29);
+      this.button1.TabIndex = 4;
+      this.button1.Text = "button1";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(688, 430);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.textBox1);
+      this.Controls.Add(this.stopButton);
+      this.Controls.Add(this.startButton);
+      this.Controls.Add(this.label1);
+      this.Name = "Form1";
+      this.Text = "Графический таймер";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Button startButton;
+    private System.Windows.Forms.Button stopButton;
+    private System.Windows.Forms.Timer timer1;
+    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.Button button1;
+  }
+}
+
---
+++ TimerGUI/TimerGUI/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimerGUI
+{
+  public partial class Form1 : Form
+  {
+    DateTime saveTime = new DateTime();
+
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      startButton.Visible = true;
+      stopButton.Visible = false;
+    }
+
+    private void timer1_Tick(object sender, EventArgs e)
+    {
+
+      TimeSpan differenceTime = DateTime.Now.Subtract(saveTime);
+      DateTime diffDateTime = Convert.ToDateTime(differenceTime.ToString());
+      string str1 = textBox1.Text;
+      DateTime timeInterval = DateTime.ParseExact(str1, "mm:ss",
+                                       System.Globalization.CultureInfo.InvariantCulture);
+
+      TimeSpan outputTime = timeInterval.Subtract(diffDateTime);
+      DateTime outputDateTime = Convert.ToDateTime(outputTime.ToString());
+
+      label1.Text = outputDateTime.ToString("mm:ss");
+      DateTime zeroTime = DateTime.ParseExact("00:00", "mm:ss",
+                                 System.Globalization.CultureInfo.InvariantCulture);
+      if (zeroTime < outputDateTime)
+      {
+        timer1.Enabled = false;
+        startButton.Visible = true;
+        stopButton.Visible = false;
+        System.Media.SoundPlayer snd = new System.Media.SoundPlayer("1.wav");
+        snd.Play();
+        MessageBox.Show("TImer");
+
+      }
+    }
+
+    private void startButton_Click(object sender, EventArgs e)
+    {
+      saveTime = DateTime.Now;
+      timer1.Enabled = true;
+      startButton.Visible = false;
+      stopButton.Visible = true;
+    }
+
+    private void stopButton_Click(object sender, EventArgs e)
+    {
+      timer1.Enabled = false;
+      startButton.Visible = true;
+      stopButton.Visible = false;
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      //System.IO.Stream str = Form1.Res 
+      //  Properties.Resources.mySoundFile;
+      System.Media.SoundPlayer snd = new System.Media.SoundPlayer("1.wav");
+      snd.Play();
+    }
+  }
+}
