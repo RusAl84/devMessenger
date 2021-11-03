501577b70cab622f748962e626329fad42720b98
Rusakov Aleksey <34722284+RusAl84@users.noreply.github.com>
2021-11-03 14:12:17 +0300

    lec 6

---
+++ Lec6/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // See https://aka.ms/new-console-template for more information
+            Console.WriteLine("Hello, World!");
+            
+            //Sample 1
+            //string s1 = "hello";
+            //string s2 = "world";
+            //string s3 = $"{s1} {s2}";
+            //string s4 = s1 + " " +s2;
+            //Console.WriteLine(s3);
+            //Console.WriteLine(s4);
+
+
+            List<string> myList = new List<string>(); 
+            string s1 = "В гостях хорошо, а дома лучше";
+            Console.WriteLine(s1);
+            foreach (string item in s1.Split())
+            {
+                myList.Add(item);
+            }
+            myList.Sort();
+            string s2 = String.Join(" ",myList.ToArray());
+            Console.WriteLine(s2);
+        }
+    }
+}
